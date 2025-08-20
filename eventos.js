daa8e6a54472437b541bbbfd2827e5a051798dca
chimihch <isamar_d@proton.me>
2025-08-20 16:22:38 -0300

    commit3

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const mdiv = document.getElementById("btn1");
+
+mdiv.addEventListener("click", () => {
+  alert("Hola! Soy el div");
+});
+});
