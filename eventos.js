1a22063034eef9aca9ddeb3f7744504a205662ed
chimihch <isamar_d@proton.me>
2025-09-01 20:37:06 -0300

    event.stopPropagation

--- eventos.js
+++ eventos.js
@@ -5,4 +5,10 @@ const mdiv = document.getElementById("btn1");
 mdiv.addEventListener("click", () => {
   alert("Hola! Soy el div");
 });
+
+const button = document.querySelector("button");
+button.addEventListener("click", (event) => {
+  event.stopPropagation();
+  alert("Hola!");
+});
 });
